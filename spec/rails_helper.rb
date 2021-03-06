ecef2b601ba26342cb528006e1e798bc27633ecc
michaelhwang <michaelhwang0619@gmail.com>
2021-03-06 19:27:27 +0800

    gem: install factory_bot_rails and timecop

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,6 +39,10 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  config.include FactoryBot::Syntax::Methods
+  config.after do
+    Timecop.return
+  end
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
