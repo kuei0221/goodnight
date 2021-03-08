6276dc5465d6df4129972b4b7a27938f88e175b7
michaelhwang <michaelhwang0619@gmail.com>
2021-03-08 15:02:59 +0800

    model: add association to user and follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :sleeps, dependent: :destroy
+  has_many :followers, class_name: 'Follow', foreign_key: :following_id, dependent: :destroy
+  has_many :followings, class_name: 'Follow', foreign_key: :follower_id, dependent: :destroy
+  has_many :follower_users, through: :followers, source: :follower
+  has_many :following_users, through: :followings, source: :following
 end
