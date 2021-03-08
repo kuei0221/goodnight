87a03771cda241883c41d9f6fb6f33e30648fc3b
michaelhwang <michaelhwang0619@gmail.com>
2021-03-08 12:30:19 +0800

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name: 'michael')
+User.create(
+  [
+    { id: 1, name: 'michael' },
+    { id: 2, name: 'ivy' },
+    { id: 3, name: 'yoyo' }
+  ]
+)
+
+(1..3).each do |n|
+  Sleep.create(
+    [
+      { user_id: n, start_at: (n + 1).day.ago, end_at: n.day.ago },
+      { user_id: n, start_at: (n + 3).day.ago, end_at: (n + 2).day.ago }
+    ]
+  )
+end
