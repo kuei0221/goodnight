ee70b241f1081882123c017dbab3a667bfd526e4
michaelhwang <michaelhwang0619@gmail.com>
2021-03-08 11:18:29 +0800

    routes: create resources sleeps with index, create, update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      resources :users, only: :show do
+        resources :sleeps, only: %i[index create update]
+      end
+    end
+  end
 end
