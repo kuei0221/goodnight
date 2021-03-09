5fd4b6fd8a54d499744e959368572a8acdb8abb7
michaelhwang <michaelhwang0619@gmail.com>
2021-03-09 22:49:59 +0800

    fix: sleeps#update returns 422 if sleep is ended

--- app/controllers/api/v1/sleeps_controller.rb
+++ app/controllers/api/v1/sleeps_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class SleepsController < ApiController
       before_action :set_sleep, only: :update
+      before_action :check_sleep_ongoing, only: :update
       before_action :set_order_direction, only: :index
       before_action :check_user_sleeping, only: :create
 
@@ -40,6 +41,12 @@ module Api
         return unless current_user.sleeping?
 
         render json: { error: 'End the current sleep to start new one' }, status: :unprocessable_entity
+      end
+
+      def check_sleep_ongoing
+        return if @sleep.ongoing?
+
+        render json: { error: 'Sleep is already ended, do not overwrite it' }, status: :unprocessable_entity
       end
     end
   end
