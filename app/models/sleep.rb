8b84f3e074e9858866be8b04254c2e0a97a8976b
michaelhwang <michaelhwang0619@gmail.com>
2021-03-09 12:59:56 +0800

    model: add presence validation of start_at to sleeps

--- app/models/sleep.rb
+++ app/models/sleep.rb
@@ -1,6 +1,7 @@
 class Sleep < ApplicationRecord
   belongs_to :user
 
+  validates_presence_of :start_at
   validates_numericality_of :end_at, greater_than: :start_at, if: :start_at, allow_nil: true
 
   before_save :count_duration
